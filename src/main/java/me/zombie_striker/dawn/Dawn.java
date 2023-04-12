a4944af1fb10eb7d03529f9c48e049d6a368f9ed
ZombieStriker <dannyboyhn@yahoo.com>
2023-04-12 02:23:12 -0400

    init

---
+++ src/main/java/me/zombie_striker/dawn/Dawn.java
@@ -0,0 +1,17 @@
+package me.zombie_striker.dawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Dawn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
